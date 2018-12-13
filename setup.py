160589e00a24e9b772786e5d6329682ffebaccde
Yuchen Lu <luyuchen.paul@gmail.com>
2018-12-12 20:58:00 -0500

    make guesswhat/src a package

---
+++ setup.py
@@ -0,0 +1,10 @@
+"""
+    Created by yuchen on 12/12/18
+    Description: Make src a package to make pycharm link every module
+"""
+from setuptools import setup
+
+setup(name='guesswhat_src',
+      description='Guesswhat',
+      version='1.0',
+      packages=['src'])
